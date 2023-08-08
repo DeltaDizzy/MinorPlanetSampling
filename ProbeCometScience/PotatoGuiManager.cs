33d3ae63fdea9dac248471819a6b547db2bfddee
Emily <Emily@DESKTOP-06E1K7N>
2023-08-07 21:21:54 -0500

    Add project files.

---
+++ ProbeCometScience/PotatoGuiManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Playables;
+
+namespace ProbeCometScience
+{
+    [KSPAddon(KSPAddon.Startup.Flight, false)]
+    public class PotatoGuiManager : MonoBehaviour
+    {
+        void Start()
+        {
+            GameEvents.onVesselGoOffRails.Add(OnVesselUnpacked);
+        }
+
+        void OnVesselUnpacked(Vessel vessel)
+        {
+            //Debug.Log("handler fired!");
+            ModuleComet comet = vessel.parts[0].FindModuleImplementing<ModuleComet>();
+            if (comet != null)
+            {
+                comet.Events["TakeSampleEVAEvent"].externalToEVAOnly = false;
+                //Debug.Log($"SampleEVAEvent is {comet.Events["TakeSampleEVAEvent"].externalToEVAOnly}");
+                comet.Events["TakeSampleEVAEvent"].guiActive = true;
+                //Debug.Log($"SampleEVAEvent is {comet.Events["TakeSampleEVAEvent"].guiActive}");
+                comet.Events["TakeSampleEVAEvent"].guiActiveUnfocused = true;
+                //Debug.Log($"SampleEVAEvent is {comet.Events["TakeSampleEVAEvent"].guiActiveUnfocused}");
+                comet.Events["TakeSampleEVAEvent"].active = true;
+            }
+        }
+
+        void OnDestroy()
+        {
+            GameEvents.onVesselGoOffRails.Remove(OnVesselUnpacked);
+        }
+    }
+}
---
+++ ProbeCometScience/PotatoPatcher.cs
@@ -0,0 +1,30 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace ProbeCometScience
+{
+    [KSPAddon(KSPAddon.Startup.Instantly, true)]
+    class PotatoPatcher : MonoBehaviour
+    {
+        void Start()
+        {
+            Harmony harmony = new Harmony("probecometscience");
+            harmony.PatchAll();
+        }
+    }
+    
+    [HarmonyPatch(typeof(ScienceUtil), nameof(ScienceUtil.RequiredUsageExternalAvailable))]
+    class UsageMaskPatch
+    {
+        static bool Prefix(ref bool __result)
+        {
+            __result = true;
+            return false;
+        }
+    }
+}
