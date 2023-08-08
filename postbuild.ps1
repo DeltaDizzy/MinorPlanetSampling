d6cb2363e36af5b0cbf423b758636e7aedb51b7b
DeltaDizzy <EJBraswell@gmail.com>
2023-08-08 00:58:20 -0500

    add post-build script

---
+++ postbuild.ps1
@@ -0,0 +1,4 @@
+﻿Move-Item -Path GameData/MinorPlanetSampling/MinorPlanetSampling.dll -Destination GameData/MinorPlanetSampling.dll
+Remove-Item -Path GameData/MinorPlanetSampling/*.dll
+Remove-Item -Path GameData/MinorPlanetSampling/*.pdb
+Move-Item -Path GameData/MinorPlanetSampling.dll -Destination GameData/MinorPlanetSampling/MinorPlanetSampling.dll
